0f8f5dab1109a2c4b7bba2c33cec7a06a2e45482
Kazi Siam <kazisiam97@gmail.com>
2021-11-24 20:58:53 -0500

    finished script for data cleaning

--- Scripts/data_cleaning.py
+++ Scripts/data_cleaning.py
@@ -1,21 +1,35 @@
+# Import library
+
 import pandas as pd
 import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
 
-df = pd.read_csv("Motor_Vehicle_Collisions_-_Crashes.csv")
-df = df.sample(n=50000)
-
-columns = ["CRASH DATE", "CRASH TIME", "BOROUGH", "LATITUDE", "LONGITUDE", "NUMBER OF PERSONS INJURED",\
+def main():
+    #importing our dataset
+    df = pd.read_csv(".././Dataset/Motor_Vehicle_Collisions_-_Crashes.csv")
+    
+    #Taking 50,000 random samples from our data.
+    df = df.sample(n=50000)
+    
+    #Keeping only the required features
+    columns = ["CRASH DATE", "CRASH TIME", "BOROUGH", "LATITUDE", "LONGITUDE", "NUMBER OF PERSONS INJURED",\
           "NUMBER OF PERSONS KILLED","NUMBER OF PEDESTRIANS INJURED", "NUMBER OF PEDESTRIANS KILLED",\
           "NUMBER OF CYCLIST INJURED", "NUMBER OF CYCLIST KILLED", "NUMBER OF MOTORIST INJURED",\
           "NUMBER OF MOTORIST KILLED"]
+    
+    #Keeping only the required features in our dataset
+    df = df[columns]
 
-df = df[columns]
-df.dropna(subset=["BOROUGH"], inplace=True)
-df["LATITUDE"].fillna(df["LATITUDE"].mean(), inplace=True)
-df["LONGITUDE"].fillna(df["LONGITUDE"].mean(), inplace=True)
-df.dropna(inplace=True)
-df["LATITUDE"] = df["LATITUDE"].astype("float64")
-df["LONGITUDE"] = df["LONGITUDE"].astype("float64")
-df.to_csv("cleaned_data.csv")
+    #Filling missing values
+    df["LATITUDE"].fillna(df["LATITUDE"].mean(), inplace=True)
+    df["LONGITUDE"].fillna(df["LONGITUDE"].mean(), inplace=True)
+    df.dropna(inplace=True)
+
+    #Fixing the datatype
+    df["LATITUDE"] = df["LATITUDE"].astype("float64")
+    df["LONGITUDE"] = df["LONGITUDE"].astype("float64")
+
+    #Saving our cleaned data in a csv file
+    df.to_csv(".././Dataset/cleaned_data.csv")
+main()
